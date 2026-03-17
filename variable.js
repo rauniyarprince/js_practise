f20f6abd4df500025393117f425964771ceabad9
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-03-17 16:39:53 +0530

    learn the var concept & print in the table formate

---
+++ variable.js
@@ -0,0 +1,13 @@
+const accountID = 1230211163
+let accountEmail = "princerauniyar3000712@gmail.com" 
+var accountPassword = "Code.0333" // var is not recommended nowadays because of its scoping problems and inconsistent behavior across cases.
+accountCity = "lucknow" //you can also define var like this 
+let accountState ; //we can only inisilize 
+
+/* accountID = 5262665656  once defined can't change */
+accountEmail = "rauniyarprince@gmail.com"
+accountPassword = "dfa"
+accountCity = "gorakhpur"
+
+console.log("you can print one by one" , accountID)
+console.table([accountID,accountEmail,accountPassword,accountCity])
