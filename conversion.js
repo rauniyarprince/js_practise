ec4271db021575709a0361c1909c074e9d5959a4
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-03-17 17:58:37 +0530

    Implemented type conversion and data type checking

--- conversion.js
+++ conversion.js
@@ -4,11 +4,11 @@ let name = undefined
 let age = null
 
 let convert_number = Number(password)
-console.log(typeof(convert_number))
-console.log("oldvalue=",password,"newvalue=",convert_number)
+console.log(typeof (convert_number))
+console.log("oldvalue=", password, "newvalue=", convert_number)
 
 let convert_Number = Number(name)
-console.log(typeof(convert_number))
-console.log("oldvalue=",undefined,"newvalue=",convert_Number)
+console.log(typeof (convert_number))
+console.log("oldvalue=", undefined, "newvalue=", convert_Number)
 
 
