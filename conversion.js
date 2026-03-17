1af95e670a47b47c746fe1e38a40b01a415ee9fe
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-03-17 17:55:56 +0530

    learn the conversion & show the dataype of data

---
+++ conversion.js
@@ -0,0 +1,14 @@
+let score = 33
+let password = "234pri"
+let name = undefined
+let age = null
+
+let convert_number = Number(password)
+console.log(typeof(convert_number))
+console.log("oldvalue=",password,"newvalue=",convert_number)
+
+let convert_Number = Number(name)
+console.log(typeof(convert_number))
+console.log("oldvalue=",undefined,"newvalue=",convert_Number)
+
+
---
+++ datatype.js
@@ -0,0 +1,16 @@
+ let num = 123
+ let name = "prince"
+ let loggedin = true
+ let state = null 
+ let designation;
+
+ //number
+ //bigint
+ //boolean
+ //string
+ //null => standalone value
+ //undefined
+ //symbol => unique
+ console.table([num,name,loggedin,state,designation])
+ console.log(typeof undefined)
+ console.log(typeof (null)) // as a method 
