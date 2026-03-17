59ee2db53e22889e502c921228130fa5ce92518f
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-03-17 19:42:14 +0530

    learn the conversion operation

---
+++ conversion_Operation.js
@@ -0,0 +1,17 @@
+console.log(2 + 2);
+console.log(2 * 2);
+console.log(2 - 2);
+console.log(2 / 2);
+console.log(2 % 2);
+console.log(2 ** 2); /*power */
+
+console.log("1" + 2); //treat string because first preference is string
+console.log(1 + "2");
+console.log("1"+ 1 + 2);
+console.log(1 + 2 + "3"); //treat number because first preference is number and add this and print 
+
+console.log("type = " ,typeof(1 + 2 + "3")); 
+
+
+
+
