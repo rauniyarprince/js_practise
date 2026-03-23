dc06ce0bdf41e03d11956c79d06f4c03cf68a925
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-03-23 23:41:45 +0530

    learn advance concept

---
+++ highorderloops/forof.js
@@ -0,0 +1,18 @@
+const arr =[1,2,5,9,2]
+for (const num of arr) {
+console.log("array is :",num);
+  
+}
+const greeting = "helloworlds"
+for (const greet of greeting) {
+  console.log("string print =",greet);
+  
+}
+const arry =[1,2,5,5,9,9,6,2]
+let sum = 0;
+for (const num of arry) {
+  sum += num;
+}
+console.log( "sum of the number =",sum); 
+
+
---
+++ loops/while-_loop.js
@@ -0,0 +1,23 @@
+
+let index = 0
+  while(index < 10){
+    console.log(index)
+  index ++
+}
+
+
+const array = [ "prince","kajal", "seema","deepak","hardik", "anamika"]
+let arr = 0
+
+while(arr < array.length){
+  console.log(array[arr])
+  arr ++
+}
+
+let score = 11  // it runs because in do we can firstly print at least one 
+do{
+  console.log(score)
+  score ++
+}
+while(score <= 10);
+
