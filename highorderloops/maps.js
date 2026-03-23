a39a23ebbf36dd660884678f0995bca712990c27
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-03-23 23:45:11 +0530

    Today’s i learn the  Map and mastering different looping techniques in JavaScript

---
+++ highorderloops/maps.js
@@ -0,0 +1,71 @@
+//  Map: Map is a built-in data structure in JavaScript
+// that stores data in key-value pairs
+// Each value is stored with a unique key
+// Any type of key is allowed
+
+const map = new Map();
+// set() is used to add data
+map.set('IN', 'India');
+map.set('USA', 'United States of America');
+map.set('FR', 'France');
+// get() is used to retrieve data
+console.log(map.get('IN')); // India
+console.log(map);
+
+//  Not ideal for Map (gives array [key, value])
+for (const value of map) {
+  console.log(value); 
+}
+
+//  Correct way to iterate Map (destructuring)
+for (const [key, value] of map) {
+  console.log(key, ":", value);
+}
+
+// Difference:
+// for...of → gives values (in Map: [key, value])
+// for...in → gives keys (mainly used for objects)
+
+
+// Object iteration using for...in
+const myobject = {
+  game1: "cricket",
+  game2: "football",
+  game3: "hockey",
+};
+for (const key in myobject) {
+  console.log(key, ":", myobject[key]);
+}
+
+
+// Array iteration using for...in (not recommended)
+const programming = ["python", "datascience", "javascript", "php"];
+
+for (const key in programming) {
+  console.log(key); // prints index (0,1,2...)
+}
+
+
+// forEach loop (best for arrays)
+const coding = ["c", "java", "python",  "php"];
+
+// normal function
+coding.forEach(function (val) {
+  console.log(val);
+});
+
+// arrow function
+coding.forEach((item) => {
+  console.log(item);
+});
+
+// separate function
+function printMe(item) {
+  console.log(item);
+}
+coding.forEach(printMe);
+
+// with index and full array
+coding.forEach((item, index, arr) => {
+  console.log(item, index, arr);
+});
