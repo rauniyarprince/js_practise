f0754d5d347a597d0baf45c93ff8480bd0712434
Prince Rauniyar <princerauniyar3000712@gmail.com>
2026-04-01 23:09:56 +0530

    create the different type of promise

--- js_advance/promise.js
+++ js_advance/promise.js
@@ -49,7 +49,6 @@ resolve({username:"Chai",email:"chai@exmaple.com"})
 promiseThree.then(function(user){
   console.log(user);
 })
-
 // fourth promise
 const promiseFour = new Promise(function(resolve,reject){
   setTimeout(function(){
@@ -92,46 +91,7 @@ else{
   },1000)
 });
 // async not handle direct error
-async function consumePromiseFive(){
-  try{
-  const response = await promiseFive
-  console.log(response);
-  }
-  catch(error){
-    console.log("error in fifth promises : ", error);
-  }
-}
 
-consumePromiseFive()
- 
-// async uses
-
-// async function getAllUsers(){
-//   try{
-//   const response = await fetch('https://jsonplaceholder.typicode.com/users')
-
-//   const data = await response.json()
-//   console.log(data);
-//   }
-//   catch(error){
-//     console.log("Error:", error);
-//   }
-// }
-// getAllUsers()
-
-
-// with .then , .catch
-
-fetch('https://api.github.com/users/rauniyarprince')
-.then(function(response){
-  return response.json()
-})
-.then(function(response){
-  console.log(response);
-})
-.catch(function(error){
-  console.log(error);
-})
 
 
 
